e91a35271deef8ac2285813420c4b1a6ff46c2eb
Mariane Candian <mariane.candian@gmail.com>
2022-10-19 16:17:43 -0300

    Requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,7 +2,7 @@ import { screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Testando o componente About.js', () => {
+describe('Testando o componente FavoritePokemons.js', () => {
   test('Verifica se a página exibe uma mensagem caso não existe nenhum pokemon como favorito', () => {
     renderWithRouter(<FavoritePokemons />);
     const texto = screen.getByText('No favorite pokemon found');
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Verifica se a página contém aum h2 com um texto', () => {
+    renderWithRouter(<NotFound />);
+    const texto = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(texto).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém a imagem do pokemon chorando', () => {
+    renderWithRouter(<NotFound />);
+    const imagem = screen.getByRole('img');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imagem).toBeInTheDocument();
+    expect(imagem.src).toContain(url);
+  });
+});
