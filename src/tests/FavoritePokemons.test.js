862b1284c00101cedd8b9061d05ee249e578b56e
Mariane Candian <mariane.candian@gmail.com>
2022-10-19 16:12:40 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente About.js', () => {
+  test('Verifica se a página exibe uma mensagem caso não existe nenhum pokemon como favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const texto = screen.getByText('No favorite pokemon found');
+    expect(texto).toBeInTheDocument();
+  });
+});
